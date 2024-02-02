ccd68be52fdb621fd6887353238690d44cd13220
Christian John <59682676+ChristianJohn-Softwareentwicklung@users.noreply.github.com>
2024-02-02 14:56:29 +0100

    Update reoback.pl

--- reoback.pl
+++ reoback.pl
@@ -65,7 +65,7 @@ my $FATAL = 50000;
 
 # GLOBAL VARIABLES
 ###########################################################################
-my %config;             # Hash containing REOBack configuration data.
+my %config;             # Hash containing REOBack configuration data.C
 my $cfgFile;            # Config file passed to REOBack
 my $fstat;              # Counter and last full backup time.
 my $archFiles;          # Used for auto deletions.
@@ -81,6 +81,8 @@ my $localMount;         # Local mount path for remote back-ups.
 my $forceFull;          # Flag for Full Update
 my $logger;             # Log4perl logging instance
 my $priority;           # Console logging priority
+my $fileSize;           # ADD by CJ local file size
+my $totalSize = 0;      # ADD by CJ total size
 
 # Parse configuration and load variables to the hash table
                                           
@@ -277,9 +279,9 @@ sub archiveFile{
   # Create the tar archive.  Use this method instead of system() so that we
   # can filter out the "Removing leading `/'" messages.  '2>&1' redirects
   # error messages from tar to stdout so we can catch them.
-                                  
+  # CJ -X must before -T to exlude
   if ( $skipFile ) {
-    $tarCmd = "$config{'tarcommand'} $fileName $config{'tarfileincl'} $listName.incl  $config{'tarfileexcl'} $listName.excl";
+    $tarCmd = "$config{'tarcommand'} $fileName $config{'tarfileexcl'} $listName.excl $config{'tarfileincl'} $listName.incl";
     open PROC, "$tarCmd 2>&1|";
   }
   else {
